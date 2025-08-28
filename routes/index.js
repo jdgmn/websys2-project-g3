71aa029275f58d97fee1babf19bcdcc8129f243f
jdgmn <20140976@s.ubaguio.edu>
2025-08-28 18:13:17 +0800

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// routes/index.js
+const express = require("express");
+const router = express.Router();
+
+// home route
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home Page",
+    message: "Hello, MongoDB is connected!",
+  });
+});
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,58 @@
+// routes/users.js
+const express = require("express");
+const router = express.Router();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+// mongoDB setup
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+const dbName = "ecommerceDB";
+
+// show registration form
+router.get("/register", (req, res) => {
+  res.render("register", { title: "Register" });
+});
+
+// handle form submission
+router.post("/register", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const usersCollection = db.collection("users");
+
+    // get form data
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    // insert into mongoDB
+    await usersCollection.insertOne(newUser);
+    res.send("User registered successfully!");
+  } catch (err) {
+    console.error("Error saving user:", err);
+    res.send("Something went wrong.");
+  }
+});
+module.exports = router;
+
+// show all registered users
+router.get("/list", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const usersCollection = db.collection("users");
+    const users = await usersCollection.find().toArray();
+    res.render("users-list", { title: "Registered Users", users: users });
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.send("Something went wrong.");
+  }
+});
+
+const {ObjectId}  = require('mongodb');
+
+// show edit form
+router get('/edit/:id', async )
